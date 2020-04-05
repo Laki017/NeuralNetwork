6d82c5131c7661206252182481d42e4bfb0ebfe7
Laki017 <nuian10@gmail.com>
2020-04-05 12:51:16 +0300

    Изменен интерфейс метода обучения. Реализованы методы маштабирования и нормализации.

--- NeuralNetworks/NeuralNetwork.cs
+++ NeuralNetworks/NeuralNetwork.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Claims;
 
 namespace NeuralNetworks
 {
@@ -37,16 +38,18 @@ namespace NeuralNetworks
             }
         }
 
-        public double Learn(List<Tuple<double, double[]>> dataset, int epoch)
+        public double Learn(double[] expected, double[,] inputes, int epoch)
         {
             var error = 0.0;
 
             for (int i = 0; i < epoch; i++)
             {
-                foreach (var data in dataset)
+                for (int j = 0; j < expected.Length; j++)
                 {
-                    error += Backpropagation(data.Item1, data.Item2);
+                    var output = expected[j];
+                    var input = GetRow(inputes, j);
 
+                    error += Backpropagation(output,input);
                 }
             }
 
@@ -54,6 +57,19 @@ namespace NeuralNetworks
             return result;
 
         }
+
+        public static double[] GetRow(double[,] matrix, int row)
+        {
+            var columns = matrix.GetLength(1);
+            var array = new double[columns];
+            for (int i = 0; i < columns; i++)
+            {
+                array[i] = matrix[row, i];
+            }
+
+            return array;
+        }
+
         private double Backpropagation(double expected, params double[] inputs)
         {
             var actual = FeedForward(inputs).Output;
@@ -83,6 +99,68 @@ namespace NeuralNetworks
             }
 
             var result= difference * difference;
+            return result;
+        }
+
+        private double[,] Scalling(double[,] inputs)
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+
+            for (int column = 0; column < inputs.GetLength(1); column++)
+            {
+                var min = inputs[0, column];
+                var max = inputs[0, column];
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    var item = inputs[row, column];
+
+                    if (item < min)
+                    {
+                        min = item;
+                    }
+
+                    if (item > max)
+                    {
+                        max = item;
+                    }
+                }
+
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - min / (max - min));
+                }
+            }
+
+            return result;
+        }
+
+        private double[,] Normalization(double[,] inputs)
+        {
+            var result = new double[inputs.GetLength(0), inputs.GetLength(1)];
+
+            for (int column = 0; column < inputs.GetLength(1); column++)
+            {
+                //Среднее значение сигнала
+                var sum = 0.0;
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    sum += inputs[row, column];
+                }
+                var average = sum / inputs.GetLength(0);
+                // Стандартное квадратичное отклонение
+                var error = 0.0;
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    error += Math.Pow((inputs[row, column] - average), 2);
+                }
+
+                var standartError = Math.Sqrt(error / inputs.GetLength(0));
+
+                for (int row = 1; row < inputs.GetLength(0); row++)
+                {
+                    result[row, column] = (inputs[row, column] - average) / standartError;
+                }
+            }
             return result;
         }
 
